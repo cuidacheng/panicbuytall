7a963cb0c84a42ac4b3eb3df3adbb44fcee2a66b
cuidacheng <cuidacheng@tuniu.com>
2019-03-28 20:26:59 +0800

    create project

---
+++ README.md
@@ -0,0 +1,2 @@
+# panicbuytall
+# panicbuytall
---
+++ src/test/java/com/boot/panicbuytall/PanicbuytallApplicationTests.java
@@ -0,0 +1,16 @@
+package com.boot.panicbuytall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PanicbuytallApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
